d1795fb6c06e2e95e026c7b0069b2c8383da2ee5
Luiz Carlos de Azevedo Santana <lucca10azevedo@gmail.com>
2026-02-08 21:32:03 +0100

    fix: cleaned the tmp data

--- run_cmake.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p build # Create build directory if it doesn't exist
-cd build
-cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-13 \
-      -DCMAKE_CXX_COMPILER=/usr/bin/g++-13 \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../src
